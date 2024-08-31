8b198227a208b9a162d68e21fde3f1a5b67014cf
OhViRuS <virusonzr@gmail.com>
2024-08-31 20:29:01 +0300

    a
    labas

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 intents = discord.Intents.default()
 intents.message_content = True
 
-token = open("token.env", "r")
+#token =
 client = discord.client(intents=intents)
 
 @client.event
@@ -11,5 +11,5 @@ async def on_ready():
     print(f"labas as esu online")
 
 
-client.run(token)
+#client.run(token)
 
